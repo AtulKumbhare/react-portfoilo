8c48e7708ff9fb69243660c8f67d50e7727097f8
Atul Kumbhare <kumbhare76@gmail.com>
2020-06-26 11:07:27 +0530

    Updates

---
+++ precache-manifest.6eed3cad519a2ae1769ec1e17e783941.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1faf4e858c120ec1ed9c",
+    "url": "/react-portfoilo/static/css/main.db0ee953.chunk.css"
+  },
+  {
+    "revision": "1faf4e858c120ec1ed9c",
+    "url": "/react-portfoilo/static/js/main.1faf4e85.chunk.js"
+  },
+  {
+    "revision": "0b9036233d3fb7aac576",
+    "url": "/react-portfoilo/static/css/1.daf6ba03.chunk.css"
+  },
+  {
+    "revision": "0b9036233d3fb7aac576",
+    "url": "/react-portfoilo/static/js/1.0b903623.chunk.js"
+  },
+  {
+    "revision": "cfe0975fb8b5a740d125",
+    "url": "/react-portfoilo/static/js/runtime~main.cfe0975f.js"
+  },
+  {
+    "revision": "39ac5000d1b0a7969e038b2c2d4de1c3",
+    "url": "/react-portfoilo/static/media/Myimg.39ac5000.jpg"
+  },
+  {
+    "revision": "54533a442e9bc4b9d1a415a5107b10b9",
+    "url": "/react-portfoilo/static/media/4k-3840-x-2160-wallpapers-themefoxx (589).54533a44.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfoilo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfoilo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfoilo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfoilo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfoilo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a3483189d76a9f4c691283b845f32772",
+    "url": "/react-portfoilo/static/media/4k-3840-x-2160-wallpapers-themefoxx (99).a3483189.jpg"
+  },
+  {
+    "revision": "5e045f66978c1043a96b388bf96b26da",
+    "url": "/react-portfoilo/index.html"
+  }
+];
